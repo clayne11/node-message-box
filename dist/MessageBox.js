57ac27cde88f898eeb3bcf6487f172606f9fdd5f
Curtis Layne <curtis.s.layne@gmail.com>
2016-10-15 16:19:53 -0400

    Updated no-handlebars to be more flexible

--- dist/MessageBox.js
+++ dist/MessageBox.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _deepExtend = require('deep-extend');
 
@@ -18,18 +18,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var replacementStrings = ['genericName', 'name', 'type'];
-var replacements = replacementStrings.reduce(function (result, replacement) {
-  return _extends({}, result, _defineProperty({}, replacement, new RegExp('{{' + replacement + '}}', 'g')));
-}, {});
+var templateRe = /({{(.*?)}})/g;
 
 var compiler = function compiler(message) {
   return function (context) {
-    return Object.keys(replacements).reduce(function (result, replacement) {
-      return result.replace(replacements[replacement], context[replacement]);
-    }, message);
+    var compiledMessage = message;
+    var result = void 0;
+    while ((result = templateRe.exec(message)) !== null) {
+      // eslint-disable-line no-cond-assign
+      var variable = result[2];
+      compiledMessage = compiledMessage.replace(new RegExp('{{' + variable + '}}', 'g'), context[variable]);
+    }
+    return compiledMessage;
   };
 };
 
